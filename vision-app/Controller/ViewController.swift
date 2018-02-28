509f80aabce073fa7b171b6883e3ea7383ee5bba
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2018-02-28 15:28:21 -0800

    Setting up project

---
+++ vision-app/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app
+//
+//  Created by Daniel Stahl on 2/28/18.
+//  Copyright © 2018 Daniel Stahl. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
